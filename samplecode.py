8c53efa8d04a8e20603d5c9c27558c5bd2db8a7c
HyndaviAuropro <115532956+HyndaviAuropro@users.noreply.github.com>
2022-10-12 19:37:15 +0530

    Update samplecode.py

--- samplecode.py
+++ samplecode.py
@@ -18,7 +18,7 @@ Enter number:14
 83
 [22, 35, 52, 83]
 
-
+---------------------------------------------------------------------------------------------------------------------------------
 
 ##########second code##########
 
@@ -37,7 +37,7 @@ name : Tim
 birthday : 1990-03-10
 job : software engineer
     
-    
+-----------------------------------------------------------------------------------------------------------------------------------------------
  ##########Third code###########
  
 employees = [{
@@ -72,54 +72,59 @@ output:
  Tim Developer Sydney
  Australia
 
-
+------------------------------------------------------------------------------------------------------------------------------
 
 ##############Fifth code##############(Simple calculator)
 
-operation = input('''
-Please type  the operation:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
-''')
 
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
-while True:
+try:
+    while True:
+        num1 = input('Enter First Number: ')
 
-  if operation == '+':
-     print('{} + {} = '.format(number_1, number_2))
-     print(number_1 + number_2)
+        if num1 == 'exit':
+            break
 
-  elif operation == '-':
-     print('{} - {} = '.format(number_1, number_2))
-     print(number_1 - number_2)
+        num1 = int(num1)
+        num2 = int(input('Enter second Numebr: '))
 
-  elif operation == '*':
-     print('{} * {} = '.format(number_1, number_2))
-     print(number_1 * number_2)
+        num3 = input('Enter operation: +,-,*,/ ')
 
-  elif operation == '/':
-     print('{} / {} = '.format(number_1, number_2))
-     print(number_1 / number_2)
+        if num3 == '+':
+            print(num1 + num2)
+        elif num3 == '-':
+            print(num1-num2)
+        elif num3 == '*':
+            print(num1*num2)
+        elif num3 == '/':
+            print(num1 // num2)
 
-  calculation = input("Do you want to continue?(continue/exit):")
-  if calculation == "exit":
-      break
-    
-  
+except:
+    print('Division with zero is not possible or Strings NOT allowed')
+        
+
 
 output:
     
-    Please type  the operation:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
-*
-Enter your first number: 1
-Enter your second number: 3
-1 * 3 = 
+    Enter First Number: 11
+Enter second Numebr: 22
+Enter operation: +,-,*,/ +
+33
+Enter First Number: 1
+Enter second Numebr: 3
+Enter operation: +,-,*,/ *
 3
-Do you want to continue?(continue/exit):exit
+Enter First Number: 5
+Enter second Numebr: 4
+Enter operation: +,-,*,/ /
+1
+Enter First Number: 1
+Enter second Numebr: 2
+Enter operation: +,-,*,/ /
+0
+Enter First Number: 5
+Enter second Numebr: 0
+Enter operation: +,-,*,/ /
+Division with zero is not possible or Strings NOT allowed
+
+  --------------------------------------------------------------------------------------------------------------------------
+            
